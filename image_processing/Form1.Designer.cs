e322d440b6152ee450a39e6267f494a8641c8d83
lemueljay <lemueljayvallinas@gmail.com>
2025-03-23 16:20:33 +0800

    Add initial project files

---
+++ image_processing/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace image_processing
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            dIPToolStripMenuItem = new ToolStripMenuItem();
+            pixelCopyToolStripMenuItem = new ToolStripMenuItem();
+            greyscaleToolStripMenuItem = new ToolStripMenuItem();
+            invertToolStripMenuItem = new ToolStripMenuItem();
+            mirrorToolStripMenuItem = new ToolStripMenuItem();
+            mirrorYToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(32, 32);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, dIPToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1108, 40);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, saveToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(71, 38);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(359, 44);
+            openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(359, 44);
+            saveToolStripMenuItem.Text = "Save";
+            // 
+            // dIPToolStripMenuItem
+            // 
+            dIPToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { pixelCopyToolStripMenuItem, greyscaleToolStripMenuItem, invertToolStripMenuItem, mirrorToolStripMenuItem, mirrorYToolStripMenuItem });
+            dIPToolStripMenuItem.Name = "dIPToolStripMenuItem";
+            dIPToolStripMenuItem.Size = new Size(70, 36);
+            dIPToolStripMenuItem.Text = "DIP";
+            // 
+            // pixelCopyToolStripMenuItem
+            // 
+            pixelCopyToolStripMenuItem.Name = "pixelCopyToolStripMenuItem";
+            pixelCopyToolStripMenuItem.Size = new Size(359, 44);
+            pixelCopyToolStripMenuItem.Text = "Pixel Copy";
+            // 
+            // greyscaleToolStripMenuItem
+            // 
+            greyscaleToolStripMenuItem.Name = "greyscaleToolStripMenuItem";
+            greyscaleToolStripMenuItem.Size = new Size(359, 44);
+            greyscaleToolStripMenuItem.Text = "Greyscale";
+            // 
+            // invertToolStripMenuItem
+            // 
+            invertToolStripMenuItem.Name = "invertToolStripMenuItem";
+            invertToolStripMenuItem.Size = new Size(359, 44);
+            invertToolStripMenuItem.Text = "Invert";
+            // 
+            // mirrorToolStripMenuItem
+            // 
+            mirrorToolStripMenuItem.Name = "mirrorToolStripMenuItem";
+            mirrorToolStripMenuItem.Size = new Size(258, 44);
+            mirrorToolStripMenuItem.Text = "Mirror X";
+            mirrorToolStripMenuItem.Click += mirrorToolStripMenuItem_Click;
+            // 
+            // mirrorYToolStripMenuItem
+            // 
+            mirrorYToolStripMenuItem.Name = "mirrorYToolStripMenuItem";
+            mirrorYToolStripMenuItem.Size = new Size(258, 44);
+            mirrorYToolStripMenuItem.Text = "Mirror Y";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1108, 660);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem dIPToolStripMenuItem;
+        private ToolStripMenuItem pixelCopyToolStripMenuItem;
+        private ToolStripMenuItem greyscaleToolStripMenuItem;
+        private ToolStripMenuItem invertToolStripMenuItem;
+        private ToolStripMenuItem mirrorToolStripMenuItem;
+        private ToolStripMenuItem mirrorYToolStripMenuItem;
+    }
+}
---
+++ image_processing/Form1.cs
@@ -0,0 +1,15 @@
+namespace image_processing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mirrorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
